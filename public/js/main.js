f0b9608ffb1d0dbe39da7e81f8ac6e186eef3553
letheguyen <thenguyenle1312@gmail.com>
2022-06-29 00:48:22 +0700

    handle Create New Feature

--- public/js/main.js
+++ public/js/main.js
@@ -1,29 +1,45 @@
 const $ = document.querySelector.bind(document)
 const $$ = document.querySelectorAll.bind(document)
+const body = $('body')    
+const Myform = $('.my')
 const from = $('.from')
+const htmls = $('.root')
+const error = $$('.error')
 const input = $$('.input')
-const inputEmail = $('.input[name="email"]')
-const inputPhone = $('.input[name="phone"]')
+const inputChange  = $$('.test') 
+const wraperFile = $('.load-avata')
 const inputRadio = $$('.input-radio')
 const inputSelect = $('.input-select')
-const error = $$('.error')
-const errorSelect = $('.error[name="select"]')
-const errorradio = $('.error[name="radio"]')
+const inputFile = $('input[name="file"]')
 const inputCheckBox = $$('.input-checkbox')
-const inputChange  = $$('.test') 
-const htmls = $('.root')
-const Myform = $('.my')
-const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const inputEmail = $('.input[name="email"]')
+const inputPhone = $('.input[name="phone"]')
+const errorRadio = $('.error[name="radio"]')
+const errorSelect = $('.error[name="select"]')
+const errorFile = $('.error[name="file"]')
+const placeholderInputFile = $('.load-avata-placeholder')
 const regNumber = /^(0[234][0-9]{8}|1[89]00[0-9]{4})$/;
+const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
 let MyCv = {interests: []}
 
-
+window.onkeydown = (e) => {
+    if(e.keyCode === 123){
+        e.preventDefault()
+    }
+}
+body.oncontextmenu = (e) => {
+    e.preventDefault()
+}
+body.onselectstart = (e) =>{
+    e.preventDefault()
+}
 from.onsubmit = (e) => {
     e.preventDefault()
     var checkRadio = 0  
     var NumberNext = 0  
     input.forEach((element, index) => {
         const eror = error[index]
+        console.log(element.value)
         if(element.value.trim() == ''){
             eror.innerHTML = 'Không được để trống trường này !'
         }else {
@@ -63,15 +79,21 @@ from.onsubmit = (e) => {
     }
     inputRadio.forEach((element, index) => {
         if(checkRadio == 0){
-            errorradio.innerHTML = 'Vui lòng chọn giới tính !'
+            errorRadio.innerHTML = 'Vui lòng chọn giới tính !'
         }
         if(element.checked){
             checkRadio += 1
-            errorradio.innerHTML = ''
+            errorRadio.innerHTML = ''
             MyCv.gender = element.value
         }
     })
-    if(checkRadio == 1 && NumberNext == 4){
+    if(placeholderInputFile.innerHTML == 'Chọn file'){
+        errorFile.innerHTML = 'Vui lòng chọn 1 hình ảnh !'
+    }else{
+        errorFile.innerHTML = ''
+        NumberNext += 1
+    }
+    if(checkRadio == 1 && NumberNext == 5){
         inputCheckBox.forEach((element,index)=> {
             if(element.checked){
                 MyCv.interests.push(inputCheckBox[index].value)
@@ -81,9 +103,16 @@ from.onsubmit = (e) => {
         htmls.classList.add('active')
     }
 }
-
+inputFile.onchange = (e) => {
+    const file =  e.target.files[0]
+    file.src = URL.createObjectURL(file) 
+    MyCv.avata = file.src
+    placeholderInputFile.innerHTML = e.target.files[0].name
+    errorFile.innerHTML = ''
+}
 function AddNewMyHtml(MyCv) {
-    Myform.innerHTML = `<div class="img" style="background-image: url(https://cdn.24h.com.vn/upload/1-2022/images/2022-03-13/anh-1-1647162766-197-width650height741.jpg);"></div>
+    console.log(MyCv)
+    Myform.innerHTML = `<div class="img" style="background-image: url(${MyCv.avata});"></div>
     <div class="information"><p>Họ và tên :</p><h3>${MyCv.name}</h3></div>
     <div class="information"><p>Email :</p><h3>${MyCv.email}</h3></div>
     <div class="information"><p>Số Điện Thoại :</p><h3>${MyCv.phone}</h3></div>
@@ -97,3 +126,7 @@ inputChange.forEach((element, index)=> {
        err.innerHTML = ''
     }
 })
+wraperFile.onclick = () => {
+    errorFile.innerHTML = ''
+    inputFile.click()
+}
